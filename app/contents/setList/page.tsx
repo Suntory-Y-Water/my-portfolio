a6900b44109169415da25a9393b3b1e375736a90
sui <1202ayasnppk@gmail.com>
2023-11-27 23:52:37 +0900

    add: SVGの追加、仮のjsonデータの追加、ライブ一覧ページのリンク作成

--- app/contents/setList/page.tsx
+++ app/contents/setList/page.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import lives from '../../../data/liveName.json';
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react';
 
 const SetList = () => {
   return (
-    <div>
-      <h1>ライブ一覧</h1>
-      <ul>
+    <div className='px-4'>
+      <h1 className='pb-4 text-navy-blue font-bold text-2xl'>ライブ一覧</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {lives.map((live) => (
-          <li key={live.id}>{live.name}</li>
+          <div
+            key={live.id}
+            className='rounded-lg p-8 text-slate-500 border-2 text-center border-slate-500 hover:border-navy-blue hover:text-navy-blue'
+          >
+            <Link href={`/contents/setList/${live.id}`}>
+              <div className='flex justify-center items-center'>
+                <h2 className='text-lg font-bold mr-2'>{live.name}</h2>
+                <ArrowRight size={20} />
+              </div>
+            </Link>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
