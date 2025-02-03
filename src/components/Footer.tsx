a1195c4463e98623a1ff6bd2d8eaf8a2bb3d2c32
sui <1202ayasnppk@gmail.com>
2025-02-04 07:59:28 +0900

    chore: font改善

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,8 @@
 export default function Footer() {
   return (
     <footer className='border-t text-sm md:h-24'>
-      <div className='mx-auto max-w-[1024px] flex h-full flex-col justify-center gap-4 p-4'>
+      <div className='mx-auto max-w-[1024px] flex h-full flex-col justify-center gap-4 p-4 text-center'>
         <p>© {new Date().getFullYear()} - Copyright スイ, All Rights Reserved.</p>
-        <p>
-          このサイトは Google Analytics を使用しています。詳しくは
-          <a
-            className='underline hover:opacity-75'
-            href='https://policies.google.com/technologies/partner-sites?hl=ja'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Google のサービスを使用するサイトやアプリから収集した情報の Google による使用
-            – ポリシーと規約
-          </a>
-          をご覧ください。
-        </p>
       </div>
     </footer>
   );
