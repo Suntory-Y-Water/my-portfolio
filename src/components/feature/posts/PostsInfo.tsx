564c798e2a6fbdd73da49338dc48da08e3d9d5ae
sui <1202ayasnppk@gmail.com>
2025-03-16 13:05:14 +0900

    feat: postsのアニメーション

--- src/components/feature/posts/PostsInfo.tsx
+++ src/components/feature/posts/PostsInfo.tsx
@@ -15,15 +15,15 @@ export default function PostsInfo({ post }: Props) {
         href={post.url}
         target='_blank'
         rel='noopener noreferrer'
-        className='flex w-full h-full flex-col items-center justify-center gap-4 p-6 border rounded-3xl hover:outline-primary hover:outline-2 hover:outline hover:bg-muted/90'
+        className='flex w-full h-full flex-col items-center justify-center gap-4 p-6 border rounded-3xl hover:outline-primary hover:outline-2 hover:outline hover:bg-muted/90 transform transition-transform duration-300 hover:scale-105'
       >
         {/* Zennのときだけ絵文字を表示する。 */}
         {post.source === 'Zenn' ? (
           <span className='text-6xl'>{post.emoji}</span>
         ) : post.source === 'Qiita' ? (
-          <Image src='/qiita-icon.svg' alt='Qiita icon' width='50' height='50' />
+          <Image src='/qiita-icon.svg' alt='Qiita icon' width='60' height='60' />
         ) : (
-          <Image src='/note-icon.svg' alt='note icon' width='50' height='50' />
+          <Image src='/note-icon.svg' alt='note icon' width='60' height='60' />
         )}
         <p className='line-clamp-2 overflow-hidden break-all text-left font-medium'>
           {post.title}
