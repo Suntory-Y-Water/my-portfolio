137d84935c7a0b57a915bf22ac4550dbea982607
sui <1202ayasnppk@gmail.com>
2025-03-22 16:51:51 +0900

    chore: ドメイン名変更

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,8 +9,8 @@ export const siteConfig: SiteConfig = {
   name: 'sui-portfolio',
   description:
     'スイのポートフォリオです。簡単な自己紹介と今まで投稿してきた記事のリンクをまとめています。',
-  url: 'https://sui-portfolio.vercel.app/',
-  ogImage: 'https://sui-portfolio.vercel.app/opengraph-image.png',
+  url: 'https://suntory-n-water.com/',
+  ogImage: 'https://suntory-n-water.com/opengraph-image.png',
   links: {
     twitter: 'https://x.com/Suntory_N_Water',
     github: 'https://github.com/Suntory-Y-Water',
